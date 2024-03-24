5612a70d59d2526c64effb649e8783e36b6a967a
Siddhesh Pokharkar <143425825+SiddheshPok@users.noreply.github.com>
2024-03-24 19:00:12 +0530

    Add files via upload

---
+++ stack.c
@@ -0,0 +1,84 @@
+#include<stdio.h>
+#include<stdlib.h>
+ 
+struct stack{
+    int size ;
+    int top;
+    int * arr;
+};
+ 
+int isEmpty(struct stack* ptr){
+    if(ptr->top == -1){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+}
+ 
+int isFull(struct stack* ptr){
+    if(ptr->top == ptr->size - 1){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+// Push the elements in the stack
+void push(struct stack* ptr, int val){
+    if(isFull(ptr)){
+        printf("Stack Overflow! Cannot push %d to the stack\n", val);
+    }
+    else{
+        ptr->top++;
+        ptr->arr[ptr->top] = val;
+    }
+}
+// Push the elements in the stack 
+int pop(struct stack* ptr){
+    if(isEmpty(ptr)){
+        printf("Stack Underflow! Cannot pop from the stack\n");
+        return -1;
+    }
+    else{
+        int val = ptr->arr[ptr->top];
+        ptr->top--;
+        return val;
+    }
+}
+ 
+int main(){
+    struct stack *sp = (struct stack *) malloc(sizeof(struct stack));
+    sp->size = 10;
+    sp->top = -1;
+    sp->arr = (int *) malloc(sp->size * sizeof(int));
+    printf("Stack has been created successfully\n");
+
+
+    printf("Before pushing, Full: %d\n", isFull(sp));
+    printf("Before pushing, Empty: %d\n", isEmpty(sp));
+
+    push(sp, 1);
+    push(sp, 23);
+    push(sp, 99);
+    push(sp, 75);
+    push(sp, 3);
+    push(sp, 64);
+    push(sp, 57);
+    push(sp, 46);
+    push(sp, 89);
+    push(sp, 6);
+
+    
+    
+
+    printf("After, Full: %d\n", isFull(sp));
+    printf("After pushing, Empty: %d\n", isEmpty(sp));
+
+    printf("popped %d from the stack \n",pop(sp));
+    printf("popped %d from the stack \n",pop(sp));
+    printf("popped %d from the stack \n",pop(sp));
+    printf("popped %d from the stack \n",pop(sp));
+ 
+    return 0;
+}
